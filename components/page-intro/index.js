7255e055701b13430bf226f53e745e880baaa66f
Lucas-Pulliese <lucaspulliese10@gmail.com>
2020-08-23 21:05:37 -0300

    Refactorized swiper sliders

--- components/page-intro/index.js
+++ components/page-intro/index.js
@@ -1,38 +1,34 @@
-import Swiper from 'react-id-swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {EffectFade, Navigation} from 'swiper';
+
+SwiperCore.use([EffectFade, Navigation]);
 
 const PageIntro = () => {
-  const params = {
-    effect: 'fade',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  }
 
   return (
     <section className="page-intro">  
-      <Swiper {...params} className="swiper-wrapper">
-        <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
-          <div className="container">
-            <div className="page-intro__slide__content">
-              <h2>Sale of the summer collection</h2>
-              <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+      <Swiper navigation effect="fade" className="swiper-wrapper">
+        <SwiperSlide>
+          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+            <div className="container">
+              <div className="page-intro__slide__content">
+                <h2>Sale of the summer collection</h2>
+                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+              </div>
             </div>
           </div>
-        </div>
+        </SwiperSlide>
 
-        <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-2.jpg')" }}>
-          <div className="container">
-            <div className="page-intro__slide__content">
-              <h2>Make your house into a home</h2>
-              <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+        <SwiperSlide>
+          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-2.jpg')" }}>
+            <div className="container">
+              <div className="page-intro__slide__content">
+                <h2>Make your house into a home</h2>
+                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+              </div>
             </div>
           </div>
-        </div>
+        </SwiperSlide>
       </Swiper>
 
       <div className="shop-data">
---
+++ pages/products.js
@@ -0,0 +1,13 @@
+import Layout from '../layouts/Main';
+import Footer from '../components/footer';
+
+const Products = () => (
+  <Layout>
+    <div>About</div>
+
+    <Footer />
+  </Layout>
+)
+  
+export default Products
+  
