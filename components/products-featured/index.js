4dde82c450c893e844a2d46fa4905f4e3fd17e6b
Lucas-Pulliese <lucaspulliese10@gmail.com>
2020-08-25 00:43:29 -0300

    changed swiper to products featured

--- components/products-featured/index.js
+++ components/products-featured/index.js
@@ -1,15 +1,7 @@
 import Product from '../Product';
-import Swiper from 'react-id-swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const ProductsFeatured = () => {
-  const params = {
-    slidesPerView: 4,
-    spaceBetween: 65,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  }
 
   return (
     <section className="section section-products-featured">
@@ -20,11 +12,19 @@ const ProductsFeatured = () => {
         </header>
 
         <div className="products-carousel">
-          <Swiper {...params} className="swiper-wrapper">
-            <div><Product discount={30} productImage={'/images/products/product-1.jpg'} /></div>
-            <div><Product productImage={'/images/products/product-2.jpg'} /></div>
-            <div><Product productImage={'/images/products/product-3.jpg'} /></div>
-            <div><Product productImage={'/images/products/product-4.jpg'} /></div>
+          <Swiper spaceBetween={65} slidesPerView={4} className="swiper-wrapper">
+            <SwiperSlide>
+              <Product discount={30} productImage={'/images/products/product-1.jpg'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Product productImage={'/images/products/product-2.jpg'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Product productImage={'/images/products/product-3.jpg'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Product productImage={'/images/products/product-4.jpg'} />
+            </SwiperSlide>
           </Swiper>
         </div>
       </div>
