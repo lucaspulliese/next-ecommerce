8b632315b5af0b94e815f60517568ef023c763d3
Lucas-Pulliese <lucaspulliese10@gmail.com>
2020-08-25 19:40:39 -0300

    WIP products

--- components/products-content/index.js
+++ components/products-content/index.js
@@ -1,4 +1,4 @@
-import Product from '../product';
+import Product from './../product';
 
 const ProductsContent = () => (
   <section className="products-content">
--- components/products-featured/index.js
+++ components/products-featured/index.js
@@ -1,4 +1,4 @@
-import Product from '../Product';
+import Product from './../product';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const ProductsFeatured = () => {
