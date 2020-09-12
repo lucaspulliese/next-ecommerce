1ec4d665c98d8f1f9387646f4fe7a48e375bbfd8
Lucas-Pulliese <lucaspulliese10@gmail.com>
2020-09-12 18:35:42 -0300

    WIP cart

--- components/shopping-cart/index.js
+++ components/shopping-cart/index.js
@@ -1,26 +1,87 @@
 const ShoppingCart = () => (
   <section className="cart">
     <div className="container">
-      <h3 className="cart__title">Shopping Cart</h3>
+      <div className="cart__intro">
+        <h3 className="cart__title">Shopping Cart</h3>
+      </div>
 
       <div className="cart-list">
         <table>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Age</th>
+            <th style={{textAlign: 'left'}}>Product</th>
+            <th>Color</th>
+            <th>Size</th>
+            <th>Ammount</th>
+            <th>Price</th>
+            <th></th>
           </tr>
+
           <tr>
-            <td>Jill</td>
-            <td>Smith</td>
-            <td>50</td>
+            <td>
+              <div className="cart-product">
+                <div className="cart-product__img">
+
+                </div>
+
+                <div className="cart-product__content">
+                  <h3>T-Shirt Summer Vibes</h3>
+                  <p>#261311</p>
+                </div>
+              </div>
+            </td>
+            <td width="15%">White</td>
+            <td width="15%">XL</td>
+            <td width="30%">
+              <div className="quantity-button">
+                <button type="button" className="quantity-button__btn">
+                  -
+                </button>
+                <span>1</span>
+                <button type="button" className="quantity-button__btn">
+                  +
+                </button>
+              </div>
+            </td>
+            <td>$89.99</td>
+            <td>$89.99</td>
           </tr>
+
           <tr>
-            <td>Eve</td>
-            <td>Jackson</td>
-            <td>94</td>
+            <td>
+              <div className="cart-product">
+                <div className="cart-product__img">
+
+                </div>
+
+                <div className="cart-product__content">
+                  <h3>T-Shirt Summer Vibes</h3>
+                  <p>#261311</p>
+                </div>
+              </div>
+            </td>
+            <td width="15%">White</td>
+            <td width="15%">XL</td>
+            <td width="30%">
+              <div className="quantity-button">
+                <button type="button" className="quantity-button__btn">
+                  -
+                </button>
+                <span>1</span>
+                <button type="button" className="quantity-button__btn">
+                  +
+                </button>
+              </div>
+            </td>
+            <td>$89.99</td>
+            <td>$89.99</td>
           </tr>
+        
         </table> 
+      </div>
+    
+      <div className="cart-actions">
+        <button type="button" className="cart__btn-back">Continue Shopping</button>
+        <input type="text" placeholder="Promo Code" className="cart__promo-code" />
       </div>
     </div>
   </section>
