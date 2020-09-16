86f40d908b8ca4511d46cac964661af0a01656df
Lucas-Pulliese <lucaspulliese10@gmail.com>
2020-09-16 12:25:14 -0300

    WIP products from json

--- utils/data/products.js
+++ utils/data/products.js
@@ -4,29 +4,360 @@ export const products = [
     name: 'T-Shirt Summer Vibes',
     price: 119.99,
     discount: 30,
+    quantityAvailable: 2,
     category: 'T-shirt',
     currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
     colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-1.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
   {
     id: '2',
-    color: '#6F3E18',
+    name: 'T-Shirt Summer Vibes',
+    price: 119.99,
+    discount: 30,
+    quantityAvailable: 2,
+    category: 'T-shirt',
+    currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
+    colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-2.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
   {
     id: '3',
-    color: '#D4BE8D',
+    name: 'T-Shirt Summer Vibes',
+    price: 119.99,
+    discount: 30,
+    quantityAvailable: 2,
+    category: 'T-shirt',
+    currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
+    colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-3.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
   {
     id: '4',
-    color: '#838383',
+    name: 'T-Shirt Summer Vibes',
+    price: 119.99,
+    discount: 30,
+    quantityAvailable: 2,
+    category: 'T-shirt',
+    currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
+    colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-4.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
   {
     id: '5',
-    color: '#F3F3F3',
+    name: 'T-Shirt Summer Vibes',
+    price: 119.99,
+    discount: 30,
+    quantityAvailable: 2,
+    category: 'T-shirt',
+    currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
+    colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-5.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
   {
     id: '6',
-    color: '#0F73AD',
+    name: 'T-Shirt Summer Vibes',
+    price: 119.99,
+    discount: 30,
+    quantityAvailable: 2,
+    category: 'T-shirt',
+    currentPrice: 89.99,
+    sizes: ['xl', 'l'], 
+    colors: ['#000', '6F3E18', '#D4BE8D', '#FFF'],
+    images: [
+      '/images/products/product-6.jpg',
+    ],
+    punctuationReview: {
+      countOpionions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 2
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 4
+      },
+      {
+        name: 'John Doe',
+        avatar: '/images/featured-1.jpg',
+        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
+        punctuation: 5
+      },
+    ]
   },
 ];
 
