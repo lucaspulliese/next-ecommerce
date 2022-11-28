329fdd0d0c21959ce0daa45c0ebddd20a0401561
Aldrin <aldrinpvincent@gmail.com>
2022-11-28 23:52:25 +0530

    sdk

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -25,7 +25,17 @@ export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
           }
       />
 
-  <Script id="moe-sdk">
+<Script dangerouslySetInnerHTML={{__html:`(function(i,s,o,g,r,a,m,n){i.moengage_object=r;t={};q=function(f){return function(){(i.moengage_q=i.moengage_q||[]).push({f:f,a:arguments})}};f=['track_event','add_user_attribute','add_first_name','add_last_name','add_email','add_mobile','add_user_name','add_gender','add_birthday','destroy_session','add_unique_user_id','moe_events','call_web_push','track','location_type_attribute'],h={onsite:["getData","registerCallback"]};for(k in f){t[f[k]]=q(f[k])}for(k in h)for(l in h[k]){null==t[k]&&(t[k]={}),t[k][h[k][l]]=q(k+"."+h[k][l])}a=s.createElement(o);m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);i.moe=i.moe||function(){n=arguments[0];return t};a.onload=function(){if(n){i[r]=moe(n)}}})(window,document,'script','https://cdn.moengage.com/webpush/moe_webSdk.min.latest.js','Moengage')`}}>
+        </Script>
+
+        <Script dangerouslySetInnerHTML={{__html:`
+          Moengage = moe({
+            app_id:"KU1MKAGZROCE4NQ1VO0VGJLO",
+            debug_logs: 0
+          });`}}>
+        </Script>
+
+  {/* <Script id="moe-sdk">
     (function (i, s, o, g, r, a, m, n) {
       i.moengage_object = r; t = {}; q = function (f) { return function () { (i.moengage_q = i.moengage_q || []).push({ f: f, a: arguments }) } }; f = ['track_event', 'add_user_attribute', 'add_first_name', 'add_last_name', 'add_email', 'add_mobile', 'add_user_name', 'add_gender', 'add_birthday', 'destroy_session', 'add_unique_user_id', 'moe_events', 'call_web_push', 'track', 'location_type_attribute'], h = { onsite: ["getData", "registerCallback"] }; for (k
         in f) { t[f[k]] = q(f[k]) } for (k in h) for (l in h[k]) { null == t[k] && (t[k] = {}), t[k][h[k][l]] = q(k + "." + h[k][l]) } a = s.createElement(o); m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m); i.moe = i.moe || function () { n = arguments[0]; return t }; a.onload = function () { if (n) { i[r] = moe(n) } }
@@ -36,7 +46,8 @@ export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
       app_id: "KU1MKAGZROCE4NQ1VO0VGJLO", // change the app ID
       debug_logs: 0
     });
-    </Script>
+
+    </Script> */}
       </Head>
       <Header />
 
